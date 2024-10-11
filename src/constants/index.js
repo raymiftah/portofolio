f3cec3917543ed3afd2667fe6f413b744bfef29b
unknown <fadlan.jerad@gmail.com>
2024-10-11 09:30:39 +0700

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,80 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `Saya sebagai Murid Produktif jurusan Rekayasa Perangkat Lunak (RPL) di salah satu SMK di
+Kota Bandung yang bernama SMK Medikacom. Saya mengajar sudah berjalan selama 2 tahun dengan mata pelajaran Pemrograman Web &
+Pemrograman Android. Saya juga berstatus sebagai Pegawai Tetap Yayasan dengan posisi Staf IT Sistem Informasi Manajemen (SIM)
+dengan jobdesk melakukan pengolahan data-data internal sekolah seperti data siswa, data pembayaran, data guru, data absensi,
+serta data kurikulum`;
+
+export const ABOUT_TEXT = `Halo! Nama saya Raihan, seorang mahasiswa di bidang Rekayasa Perangkat Lunak yang berasal dari [kota atau daerah. Saya memiliki ketertarikan yang mendalam dalam pengembangan perangkat lunak dan teknologi informasi, dan saya selalu berusaha untuk mengasah keterampilan saya dalam berbagai bahasa pemrograman, seperti sebutkan beberapa bahasa pemrograman yang dikuasai, seperti "Java", "Python", "JavaScript". Sejak memulai studi di unervisitas, saya terlibat dalam beberapa proyek, mulai dari aplikasi mobile hingga sistem manajemen basis data, yang memungkinkan saya untuk menerapkan teori yang saya pelajari dan bekerja dalam tim yang dinamis. Di luar akademik, saya aktif dalam sebutkan kegiatan ekstrakurikuler atau organisasi, seperti "klub pemrograman" atau "komunitas teknologi", yang membantu saya terhubung dengan para profesional dan sesama penggemar teknologi. Selain itu, saya juga sangat menyukai hobi atau minat lain, seperti "bermain game", "membaca buku tentang teknologi", "berolahraga", yang membantu saya menjaga keseimbangan antara kerja dan kehidupan pribadi. Saya percaya bahwa inovasi terbaik muncul dari kolaborasi, dan saya berharap dapat terus belajar serta berkontribusi positif di bidang perangkat lunak di masa depan. Terima kasih telah membaca!`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Now",
+    role: "Web Developer",
+    company: "Desain Kreasi Jasa Website Bandung",
+    description: `Web Developer adalah profesional yang merancang, membangun, dan memelihara situs web. Mereka dibagi menjadi dua kategori utama: Frontend Developer, yang fokus pada tampilan dan interaksi pengguna menggunakan HTML, CSS, dan JavaScript, dan Backend Developer, yang mengelola logika server dan basis data dengan bahasa seperti PHP, Python, atau Node.js. Beberapa web developer juga berperan sebagai Full Stack Developer, yang mampu menangani kedua aspek`,
+    technologies: ["PHP", "Native", "HTML", "MySQL"],
+  },
+  {
+    year: "tahun 2019 - 2022",
+    role: "Mobile App Developer",
+    company: "Jakarta",
+    description: `Mobile app developer menggunakan berbagai bahasa pemrograman dan alat pengembangan. Untuk Android, mereka sering menggunakan Java atau Kotlin, sedangkan untuk iOS, bahasa yang umum digunakan adalah Swift atau Objective-C. Selain itu, ada juga framework seperti React Native atau Flutter yang memungkinkan pengembangan aplikasi lintas platform.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Full Stack Developer",
+    company: "Facebook",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Software Engineer",
+    company: "Paypal",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "jl.cijambe komp.pasirjati residence no 18 G  ",
+  phoneNo: "+62 829 418 803 8 ",
+  email: "raihanmiftah9@gmail.com ",
+};
